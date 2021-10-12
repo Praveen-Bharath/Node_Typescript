bb4bf844d54f0c530a43aaaeda42d1c3d5c1eed5
Praveen-Bharath <praveen.b@tringapps.com>
2021-10-12 11:08:51 +0530

    node

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const add = (a: number, b: number): number => a+b;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     console.log(add(5,5))
-    res.send('Hello');
+    res.send('Hello All');
 
 });
 
